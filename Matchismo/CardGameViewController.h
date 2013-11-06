2dacf35480633f8f636bfa63e24d996e289a92ad
Richard Millet <remillet@yahoo.com>
2013-11-06 13:14:56 -0800

    Initial commit.

---
+++ Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Richard E Millet on 11/6/13.
+//  Copyright (c) 2013 remillet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
---
+++ Matchismo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Matchismo
+//
+//  Created by Richard E Millet on 11/6/13.
+//  Copyright (c) 2013 remillet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CardGameAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([CardGameAppDelegate class]));
+	}
+}
